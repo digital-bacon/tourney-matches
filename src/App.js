fa9d09996886699508d56ce9c11ec5debbbb6124
Mark Bieganek <mark.bieganek@telus.com>
2023-01-08 11:20:35 -0700

    Import MatchList, PlayerList and call in function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import MatchList from './components/MatchList';
+import PlayerList from './components/PlayerList';
 
 function App() {
   return (
-    null
+    <div className="App">
+      <h1>
+        Tourney Matches{" "}
+        <span>Where Coding and Tournaments found their Match!</span>
+      </h1>
+      <PlayerList />
+      <MatchList />
+    </div>
   );
 }
 
