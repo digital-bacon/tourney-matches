b870bf17ea4d70b28482fbf9128ee5e4c15636ea
Mark Bieganek <mark.bieganek@telus.com>
2023-01-08 10:44:39 -0700

    Improve JSX readability, add line breaks

--- src/components/Match.js
+++ src/components/Match.js
@@ -2,8 +2,10 @@ function Match() {
   return (
     <section className="Match">
       <h1>Jow <span>vs</span> Bob</h1>
-      {/* To be shown when there is a winner */}<h2>Joe is the winner by 1!</h2>
-      {/* To be shown when there is no winner */}<h2>No winners yet!</h2>
+      {/* To be shown when there is a winner */}
+      <h2>Joe is the winner by 1!</h2>
+      {/* To be shown when there is no winner */}
+      <h2>No winners yet!</h2>
     </section>
   );
 }
