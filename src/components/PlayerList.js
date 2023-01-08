f9b71d60276b2e6c59f028b94b9bdc726d9739d8
Mark Bieganek <mark.bieganek@telus.com>
2023-01-08 15:24:08 -0700

    Refactor data import to parent App.js

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,12 +1,9 @@
-import matchData from '../data/matchData';
-import playerData from '../data/playerData';
 import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers'
 import Player from './Player';
 
 function PlayerList(props) {
-  const playerDataArray = preparePlayerData(playerData);
-  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
-  console.log(parsedPlayerData)
+  const playerDataArray = preparePlayerData(props.playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, props.matchData);
   const onePlayer = parsedPlayerData[0];
   return (
     <section className="PlayerList">
