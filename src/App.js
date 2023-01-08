6cf990566c22c88f58307879f8df97188bc614b7
Mark Bieganek <mark.bieganek@telus.com>
2023-01-08 15:33:53 -0700

    Refactor helpers import to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
-import matchData from './data/matchData';
-import playerData from './data/playerData';
 import MatchList from './components/MatchList';
 import PlayerList from './components/PlayerList';
+import matchData from './data/matchData';
+import playerData from './data/playerData';
+import { preparePlayerData, addWinsToPlayers } from './helpers/playerHelpers'
 
 function App() {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
   return (
     <div className="App">
       <h1>
         Tourney Matches{" "}
         <span>Where Coding and Tournaments found their Match!</span>
       </h1>
-      <PlayerList matchData={matchData} playerData={playerData} />
+      <PlayerList matchData={matchData} playerData={parsedPlayerData} />
       <MatchList matchData={matchData} />
     </div>
   );
