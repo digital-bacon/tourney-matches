761e3a1635c56a4a74c1549782ba4e1c97822406
Mark Bieganek <mark.bieganek@telus.com>
2023-01-08 13:50:19 -0700

    Include data + helpers

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,6 +1,9 @@
+import matchData from '../data/matchData';
+import playerData from '../data/playerData';
+import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers'
 import Player from './Player';
 
-function PlayerList() {
+function PlayerList(props) {
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
